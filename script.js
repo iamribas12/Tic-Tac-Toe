40124832f611c61f6acc3cba2af1400ae5da9ec1
Sabir <iamribas12@gmail.com>
2024-04-29 16:08:38 +0530

    again developed

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var button = document.querySelectorAll(".box");
-var resBut = document.querySelector("#reset-btn");
-let newBtn = document.querySelector("#play-again");
-let msgCont = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg")
+const button = document.querySelectorAll(".box");
+const resBut = document.querySelector("#reset-btn");
+const newBtn = document.querySelector("#play-again");
+const msgCont = document.querySelector(".msg-container");
+const msg = document.querySelector("#msg");
 const audio = document.querySelector("#myAudio");
-var turn = true;
-var winPatterns = [
+let turn = true;
+const winPatterns = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -18,68 +18,59 @@ var winPatterns = [
 
 button.forEach((box) => {
     box.addEventListener("click", () => {
-        // console.log("box was clicked");
-        // box.classList.add("col");
         if (turn) {
             box.innerText = "X";
-            box.style.color = "red"
+            box.style.color = "red";
             turn = false;
         } else {
-            box.innerText = "O"
-            box.style.color = "blue"
+            box.innerText = "O";
+            box.style.color = "blue";
             turn = true;
         }
 
         box.disabled = true;
-        checkwinner();
+        checkWinner();
     });
 });
 
-const disableBox = () => {
-    for (let box of button) {
+const disableBoxes = () => {
+    button.forEach((box) => {
         box.disabled = true;
-    }
-}
+    });
+};
+
 const enableBoxes = () => {
-    for (let box of button) {
+    button.forEach((box) => {
         box.disabled = false;
         box.innerText = "";
-    }
+    });
+};
 
-}
-
-const checkwinner = () => {
-        for (var patterns of winPatterns) {
-            // console.log(patterns[0], patterns[1], patterns[2]);
-            // console.log(button[patterns[0]], button[patterns[1]], button[patterns[2]]);
-            var ps0 = button[patterns[0]].innerText;
-            var ps1 = button[patterns[1]].innerText;
-            var ps2 = button[patterns[2]].innerText;
-            if (ps0 != "" && ps1 != "" && ps2 != "") {
-                if (ps0 === ps1 && ps1 == ps2) {
-                    // alert(`Winner is ${ps1}`);
-                    // alert(`congratulations🎊 ${ps1}`)
-                    audio.play();
-                    shoWinner(ps1);
-                }
+const checkWinner = () => {
+    for (const patterns of winPatterns) {
+        const ps0 = button[patterns[0]].innerText;
+        const ps1 = button[patterns[1]].innerText;
+        const ps2 = button[patterns[2]].innerText;
+        if (ps0 !== "" && ps1 !== "" && ps2 !== "") {
+            if (ps0 === ps1 && ps1 === ps2) {
+                audio.play();
+                showWinner(ps1);
             }
         }
     }
-    ///////////////////////// Winner interface func
-const shoWinner = (winner) => {
-    msg.innerText = `Congratulation, winner is ${winner}`;
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, the winner is ${winner}`;
     msgCont.classList.remove("hide");
-    disableBox();
+    disableBoxes();
+};
 
-}
-
-
-const restGame = () => {
-    turn = true;
+const resetGame = () => {
     enableBoxes();
     msgCont.classList.add("hide");
-}
+    audio.pause();
+};
 
-// resBut.addEventListener("")
-newBtn.addEventListener("click", restGame);
-resBut.addEventListener("click", restGame)
+newBtn.addEventListener("click", resetGame);
+resBut.addEventListener("click", resetGame);
