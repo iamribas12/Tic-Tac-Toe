1ed6c4a63b70e684a935091f4b0f5f35688458d9
Sabir <iamribas12@gmail.com>
2024-04-29 15:27:50 +0530

    all changes are done like

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var button = document.querySelectorAll(".box");
 var resBut = document.querySelector("#reset-btn");
+let newBtn = document.querySelector("#play-again");
+let msgCont = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg")
+const audio = document.querySelector("#myAudio");
 var turn = true;
 var winPatterns = [
     [0, 1, 2],
@@ -31,19 +35,51 @@ button.forEach((box) => {
     });
 });
 
+const disableBox = () => {
+    for (let box of button) {
+        box.disabled = true;
+    }
+}
+const enableBoxes = () => {
+    for (let box of button) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+
+}
+
 const checkwinner = () => {
-    for (var patterns of winPatterns) {
-        // console.log(patterns[0], patterns[1], patterns[2]);
-        // console.log(button[patterns[0]], button[patterns[1]], button[patterns[2]]);
-        var ps0 = button[patterns[0]].innerText;
-        var ps1 = button[patterns[1]].innerText;
-        var ps2 = button[patterns[2]].innerText;
-        if (ps0 != "" && ps1 != "" && ps2 != "") {
-            if (ps0 === ps1 && ps1 == ps2) {
-                alert(`Winner is ${ps1}`);
+        for (var patterns of winPatterns) {
+            // console.log(patterns[0], patterns[1], patterns[2]);
+            // console.log(button[patterns[0]], button[patterns[1]], button[patterns[2]]);
+            var ps0 = button[patterns[0]].innerText;
+            var ps1 = button[patterns[1]].innerText;
+            var ps2 = button[patterns[2]].innerText;
+            if (ps0 != "" && ps1 != "" && ps2 != "") {
+                if (ps0 === ps1 && ps1 == ps2) {
+                    // alert(`Winner is ${ps1}`);
+                    // alert(`congratulations🎊 ${ps1}`)
+                    audio.play();
+                    shoWinner(ps1);
+                }
             }
         }
     }
+    ///////////////////////// Winner interface func
+const shoWinner = (winner) => {
+    msg.innerText = `Congratulation, winner is ${winner}`;
+    msgCont.classList.remove("hide");
+    disableBox();
+
 }
 
-// 38:39
+
+const restGame = () => {
+    turn = true;
+    enableBoxes();
+    msgCont.classList.add("hide");
+}
+
+// resBut.addEventListener("")
+newBtn.addEventListener("click", restGame);
+resBut.addEventListener("click", restGame)
